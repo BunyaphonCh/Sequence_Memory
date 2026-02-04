9ef2db028fd1f1ea06b66e5f0e6989fc2582cad9
Bell <bunyaphon536@gmail.com>
2026-02-04 23:57:35 +0700

    add onClick

--- my-game/src/App.jsx
+++ my-game/src/App.jsx
@@ -74,6 +74,7 @@ function App() {
           {[0, 1, 2, 3, 4, 5, 6, 7, 8].map((id) => (
             <button
               key={id}
+              onClick={() => handleButtonClick(id)}
               disabled={isShowingSequence} // ห้ามกดตอนไฟโชว์
               className={`aspect-square rounded-xl transition-all duration-200 
                 ${id === litButton 
